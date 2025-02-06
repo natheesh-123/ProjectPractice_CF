5a0e8dfc73a2c130b6ebfff74dd8e2244dd38cbd
natheesh-123 <natheeshkumar4656@gmail.com>
2025-02-06 09:20:29 +0530

    new

--- Attendance_management/Attendance_management/obj/Debug/net8.0/Attendance_management.AssemblyInfo.cs
+++ Attendance_management/Attendance_management/obj/Debug/net8.0/Attendance_management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Attendance_management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88dae6885bf34867047bd37e4df3f28113cdfdb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Attendance_management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Attendance_management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
