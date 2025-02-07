5fd7101adda96d0c231dfb6cb0cc62ebb62628ed
natheesh <natheeshkumar4656@gmail.com>
2025-02-07 18:44:39 +0530

    new

---
+++ Attendance_management-app/src/app/AttendanceModule/attendance-dashboard/attendance-dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance-dashboard',
+  imports: [],
+  templateUrl: './attendance-dashboard.component.html',
+  styleUrl: './attendance-dashboard.component.css'
+})
+export class AttendanceDashboardComponent {
+
+
+  
+
+}
